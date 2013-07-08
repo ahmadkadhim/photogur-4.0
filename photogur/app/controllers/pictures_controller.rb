04757610412ecdd76ed3775b0e3a2b8dabbfb5c4
AhmadKadhim <akadhim13@gmail.com>
2013-07-08 17:45:17 -0400

    Refactored CSS to properly display a and img in different pages; a tags no longer take up full width of page; fixed new picture by adding #new action in controller and fixing typo in the routes

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -9,11 +9,11 @@ class PicturesController < ApplicationController
 	end
 
 	def new
-		
+		@picture = Picture.new
 	end
 
 	def create
-		@picture = Picture.new(params[:picture])
+		@picture = Picture.new(picture_params)
 		if @picture.save
 			redirect_to pictures_url
 		else
