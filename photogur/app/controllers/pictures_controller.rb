819e5b87c3ba2a7fbbf60b4f98128490a349227a
AhmadKadhim <akadhim13@gmail.com>
2013-07-08 16:53:45 -0400

    Refactored to save pictures to database; changed caption text

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,44 +1,11 @@
 class PicturesController < ApplicationController
 
 	def index
-		@pictures = [
-			{
-				:title => "Erdogan Can't Deal",
-				:artist => "IR Cats",
-				:url => "http://25.media.tumblr.com/fb15ed8eb293ea2ec8f5aa56f60414dd/tumblr_mp9i9bKyDF1rlnfh7o1_1280.jpg"
-			},
-			{
-				:title => "Hunting Edward Snowden",
-				:artist => "IR Cats",
-				:url => "http://25.media.tumblr.com/99a59994346bab266c661ab2b895d81d/tumblr_mp0ga0Ghls1rlnfh7o1_1280.jpg"
-			},
-			{
-				:title => "American Stance on Syria",
-				:artist => "IR Cats",
-				:url => "http://25.media.tumblr.com/bb00037b12535e908f66bc0982db6a9f/tumblr_mm57q9E5kA1rlnfh7o1_1280.jpg"
-			}
-		]
+		@pictures = Picture.all
 	end
 
 	def show
-		@pictures = [
-			{
-				:title => "Erdogan Can't Deal",
-				:artist => "IR Cats",
-				:url => "http://25.media.tumblr.com/fb15ed8eb293ea2ec8f5aa56f60414dd/tumblr_mp9i9bKyDF1rlnfh7o1_1280.jpg"
-			},
-			{
-				:title => "Hunting Edward Snowden",
-				:artist => "IR Cats",
-				:url => "http://25.media.tumblr.com/99a59994346bab266c661ab2b895d81d/tumblr_mp0ga0Ghls1rlnfh7o1_1280.jpg"
-			},
-			{
-				:title => "American Stance on Syria",
-				:artist => "IR Cats",
-				:url => "http://25.media.tumblr.com/bb00037b12535e908f66bc0982db6a9f/tumblr_mm57q9E5kA1rlnfh7o1_1280.jpg"
-			}
-		]
-		@picture = @pictures[params[:id].to_i]
+		@picture = Picture.find(params[:id])
 	end
 
 	def new
@@ -46,7 +13,12 @@ class PicturesController < ApplicationController
 	end
 
 	def create
-		render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+		@picture = Picture.new(params[:picture])
+		if @picture.save
+			redirect_to pictures_url
+		else
+			render :new
+		end
 	end	
 
 
